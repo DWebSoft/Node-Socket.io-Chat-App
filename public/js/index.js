fdbf6126c40b746afcd7c335306a06d8453b526c
DWebSoft <dwebsoftguy@gmail.com>
2018-01-25 21:12:21 +0530

    changes reverted

--- public/js/index.js
+++ public/js/index.js
@@ -12,7 +12,6 @@ var unreadMessagesContainer = jQuery('.unread-messages');
 
 //Check when user scrolled to bottom
 jQuery(messageContainer).on('scroll', function(){
-    alert(messageContainer.prop('scrollTopMax'));
     if (messageContainer.prop('scrollTopMax') == messageContainer.prop('scrollTop') ){
         //Scroll to bottom
         hideUnreadMessages();
