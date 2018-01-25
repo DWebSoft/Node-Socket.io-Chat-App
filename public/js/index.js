aaeecfde610cb627445f380b17bb1f0550851aa2
DWebSoft <dwebsoftguy@gmail.com>
2018-01-25 17:53:59 +0530

    Formatted time using moment.js

--- public/js/index.js
+++ public/js/index.js
@@ -12,7 +12,8 @@ socket.on('newMessage', function(message){
 
     //Add message on screen
     var li = jQuery('<li></li>');
-    li.text(`${message.from}: ${message.text}`);
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    li.text(`${message.from} ${formattedTime}: ${message.text}`);
     jQuery('#messages').append(li);
 });
 
@@ -20,7 +21,8 @@ socket.on('newMessage', function(message){
 socket.on('newLocationMessage', function(message){
     var li = jQuery('<li></li>');
     var a = jQuery('<a target="_blank">My current location</a>');
-    li.text(`${message.from}: `);
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    li.text(`${message.from} ${formattedTime}: `);
     a.attr('href', message.url);
     li.append(a);
     jQuery('#messages').append(li);
